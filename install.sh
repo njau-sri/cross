58c5922facbcb4d3d187cfb07864bb25169f23ed
Jianbo He <hjbxyz@gmail.com>
2019-07-17 17:03:27 +0800

    Update install

--- install.sh
+++ install.sh
@@ -9,12 +9,11 @@ TOP=$(pwd)
 rm -rf $PKG
 mkdir $PKG
 
-make clean
-
 if [[ $1 == "glnx64" ]]; then
 
     make || exit 1
     cp cross $TOP/$PKG/
+    make clean
 
     cd src
     make distclean
@@ -29,6 +28,7 @@ elif [[ $1 == "win32" ]]; then
 
     make win32 || exit 1
     cp cross.exe $TOP/$PKG/
+    make clean
 
     cd src
     make distclean
@@ -41,6 +41,7 @@ elif [[ $1 == "win64" ]]; then
 
     make win64 || exit 1
     cp cross.exe $TOP/$PKG/
+    make clean
 
     cd src
     make distclean
@@ -54,6 +55,7 @@ elif [[ $1 == "macos" ]]; then
     export PATH="/usr/local/opt/llvm@7/bin:$PATH"
     make macos || exit 1
     cp cross $TOP/$PKG/
+    make clean
 
     export PATH="/usr/local/opt/qt/bin:$PATH"
     export LDFLAGS="-L/usr/local/opt/qt/lib"
